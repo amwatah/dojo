cf798673ec253c0df05b071331d333dc12d456d5
Amwatah Obuya <obuyaamwatah@gmail.com>
2023-09-21 16:20:39 +0300

    added pwa support

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,9 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;500;700;900&family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         ></link>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png"></link>
+        <meta name="theme-color" content="#fff" />
       </Head>
       <body>
         <Main />
