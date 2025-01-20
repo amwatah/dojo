422d7079518cdb1f191f6751293d7fd832c20a8d
Amwatah Obuya <obuyaamwatah@gmail.com>
2025-01-20 20:43:52 +0300

    add new projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -23,17 +23,21 @@ const Projects = () => {
         />
         <ProjectCard
           projectType="Web"
-          title="Elimu-hub"
-          description={
-            <p>
-              An online learning platform that allows Students and tutors watch video courses .Tutors are able to create courses , get payments while students
-              can buy courses and track their progress .Also features an analytics dashboard for tutors.
-            </p>
-          }
-          imageRelativePath="/projects/elimu.png"
-          technonogies={["Next Js", "Typescript", "Koa", "Postgres", "ShadcnUI"]}
-          url="https://elimu-hub.vercel.app"
-          github="https://github.com/amwatah/elimu-hub"
+          title="Safariswap"
+          description={<p>A blockchain on-ramp and off-ramp application that intergrates with M-Pesa for payments</p>}
+          imageRelativePath="/projects/safariswap.png"
+          technonogies={["Next Js", "Typescript", "M-Pesa", "Web3Js", "Solidity"]}
+          url="https://www.safariswap.io/"
+          github="https://github.com/amwatah"
+        />
+        <ProjectCard
+          projectType="Web"
+          title="E-tree"
+          description={<p>A reforestation management application for Earthlungs Reforestation ,an NGO based in Kenya and Tanzania .</p>}
+          imageRelativePath="/projects/etree.png"
+          technonogies={["Next Js", "Typescript", "Mantine", "React Query"]}
+          url="https://www.etree.app/admin/planting-sessions"
+          github="https://github.com/amwatah"
         />
         <ProjectCard
           projectType="Web"
@@ -48,6 +52,20 @@ const Projects = () => {
           technonogies={["Next", "Typescript", "Postgres", "tRPC"]}
           url="https://teksade.vercel.app"
           github="https://github.com/amwatah/teksade"
+        />
+        <ProjectCard
+          projectType="Web"
+          title="Elimu-hub"
+          description={
+            <p>
+              An online learning platform that allows Students and tutors watch video courses .Tutors are able to create courses , get payments while students
+              can buy courses and track their progress .Also features an analytics dashboard for tutors.
+            </p>
+          }
+          imageRelativePath="/projects/elimu.png"
+          technonogies={["Next Js", "Typescript", "Koa", "Postgres", "ShadcnUI"]}
+          url="https://elimu-hub.vercel.app"
+          github="https://github.com/amwatah/elimu-hub"
         />
       </div>
     </section>
